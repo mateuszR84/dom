2d6862b78e76f881ee418330c9408c62a77ef04e
Mateusz Rycombel <mateusz@init.biz>
2024-11-14 12:56:45 +0100

    initial commit

---
+++ Plugin.php
@@ -0,0 +1,86 @@
+<?php namespace StDevs\Dom;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * Plugin Information File
+ *
+ * @link https://docs.octobercms.com/3.x/extend/system/plugins.html
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * pluginDetails about this plugin.
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name' => 'Dom',
+            'description' => 'Digital media organizer',
+            'author' => 'StDevs',
+            'icon' => 'icon-archive'
+        ];
+    }
+
+    /**
+     * register method, called when the plugin is first registered.
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * boot method, called right before the request route.
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * registerComponents used by the frontend.
+     */
+    public function registerComponents()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'StDevs\Dom\Components\MyComponent' => 'myComponent',
+        ];
+    }
+
+    /**
+     * registerPermissions used by the backend.
+     */
+    public function registerPermissions()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'stdevs.dom.some_permission' => [
+                'tab' => 'Dom',
+                'label' => 'Some permission'
+            ],
+        ];
+    }
+
+    /**
+     * registerNavigation used by the backend.
+     */
+    public function registerNavigation()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'dom' => [
+                'label' => 'Dom',
+                'url' => Backend::url('stdevs/dom/mycontroller'),
+                'icon' => 'icon-leaf',
+                'permissions' => ['stdevs.dom.*'],
+                'order' => 500,
+            ],
+        ];
+    }
+}
